4ae6068537df592f9ddea23894d85bbcf43f6919
Lastonetry <alwaysoptimisticfutureiitian@gmail.com>
2024-07-07 09:32:14 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,100 @@
+import re
+import os
+from os import environ
+from Script import script
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+#main variables
+API_ID = int(environ.get('API_ID', '14766829'))
+API_HASH = environ.get('API_HASH', 'cfaca926290f395a7404927ccb88422d')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6844134599:AAGHNxpSq3w3vienvRfhSi_bACZe1pMeUlM')
+
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5281368582').split()]
+USERNAME = environ.get('USERNAME', "https://telegram.me/ThaFallenStar")
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002246672144'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+dEzWVyCkW-ZhZTEx')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002172652828').split()]
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://TheDazai:TheDazai2570@@cluster0.p1gvxvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+DATABASE_NAME = environ.get('DATABASE_NAME', "FiltronDB")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Movies_Files')
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002172652828'))
+QR_CODE = environ.get('QR_CODE', 'https://cosmofeed.com/vp/6688bf7aa2b7c30013b88eec')
+START_IMG = environ.get('START_IMG', 'https://graph.org/file/4dad0cc16f190468454ee.jpg')
+BIN_CHANNEL = int(environ.get('BIN_CHANNEL','-1002183748149'))
+DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
+URL = environ.get('URL', 'geographical-amelie-nopehaidude-807ea181.koyeb.app')
+STICKERS_IDS = ('CAACAgUAAxkBAAJj4WaIqTYra_7MqFiaN0qupUqkGWwnAAJxDQACMR_wVMpswjKZuanTNQQ CAACAgUAAxkBAAJj5GaIqUfw60Avv5g7aIHeljHoR5RdAAKFDQACSlbQVLYWdTgffJ24NQQ CAACAgUAAxkBAAJj52aIqUtuVeO_vpzQe_AIPjd12-gsAAJwDAACS7exVIuiWilCxITNNQQ CAACAgUAAxkBAAJj6maIqU202q_iFd4P8j_OMPEzgLgkAAIFEQACTC_IVCvSGvMS2Ph_NQQ').split()
+FILE_AUTO_DEL_TIMER = int(environ.get('FILE_AUTO_DEL_TIMER', '600'))
+IS_VERIFY = is_enabled('IS_VERIFY', False)
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002172652828'))
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/+efuxDsPYsmc2MGYx")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
+SHORTENER_API = environ.get("SHORTENER_API", "5bb6e402dd86fb8774690a5f4a65d2a2c0c04877")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'shortslink.in')
+SHORTENER_API2 = environ.get("SHORTENER_API2", "41a89e7a1f16e7dbec0ee52d743f3b5a38a09613")
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'shortslink2.com')
+SHORTENER_API3 = environ.get("SHORTENER_API3", "f287e7e9b1a23c34f542f77787d39607cae36a4d")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'shortslink3.online')
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
+
+LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
+QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
+YEARS = [f'{i}' for i in range(2024 , 2002,-1 )]
+SEASONS = [f'season {i}'for i in range (1 , 23)]
+REF_PREMIUM = 30
+PREMIUM_POINT = 100
+auth_channel = environ.get('AUTH_CHANNEL', '-1002153149942')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002226110484'))
+request_channel = environ.get('REQUEST_CHANNEL', '-1002183748149')
+REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
+UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002172652828'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002153149942'))
+
+AUTO_FILTER = is_enabled('AUTO_FILTER', True)
+PORT = os.environ.get('PORT', '5000')
+MAX_BTN = int(environ.get('MAX_BTN', '8'))
+AUTO_DELETE = is_enabled('AUTO_DELETE', True)
+DELETE_TIME = int(environ.get('DELETE_TIME', 1200))
+IMDB = is_enabled('IMDB', False)
+FILE_CAPTION = environ.get('FILE_CAPTION', f'{script.FILE_CAPTION}')
+IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', f'{script.IMDB_TEMPLATE_TXT}')
+LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', False)
+PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
+SPELL_CHECK = is_enabled('SPELL_CHECK', True)
+LINK_MODE = is_enabled('LINK_MODE', True)
+SETTINGS = {
+            'spell_check': SPELL_CHECK,
+            'auto_filter': AUTO_FILTER,
+            'file_secure': PROTECT_CONTENT,
+            'auto_delete': AUTO_DELETE,
+            'template': IMDB_TEMPLATE,
+            'caption': FILE_CAPTION,
+            'tutorial': TUTORIAL,
+            'shortner': SHORTENER_WEBSITE,
+            'api': SHORTENER_API,
+            'shortner_two': SHORTENER_WEBSITE2,
+            'api_two': SHORTENER_API2,
+            'log': LOG_VR_CHANNEL,
+            'imdb': IMDB,
+            'link': LINK_MODE, 
+            'is_verify': IS_VERIFY, 
+            'verify_time': TWO_VERIFY_GAP,
+            'shortner_three': SHORTENER_WEBSITE3,
+            'api_three': SHORTENER_API3,
+            'third_verify_time': THREE_VERIFY_GAP
+    }
+DEFAULT_POST_MODE = {
+    'singel_post_mode' : False,
+    'all_files_post_mode' : False
+}
